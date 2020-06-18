01d9aa18e43dc8485d253b7a19908f35ec4dbc82
farshana23 <kfarshana@gmail.com>
2020-06-18 22:13:54 +0530

    app.js change

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import {React, Component} from 'react';
-import {View, Text} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {createAppContainer, createStackNavigator} from 'react-navigation';
+import ToDoScreen from './components/ToDoScreen';
+import ToDoDetails from './components/ToDoDetails';
+import AddToDo from './components/AddToDo';
+import ToDoEdit from './components/ToDoEdit';
 
-export default class App extends Component {
+const Main = createStackNavigator(
+  {
+    ToDoScreen: ToDoScreen,
+    ToDoDetails: ToDoDetails,
+    AddToDo: AddToDo,
+    ToDoEdit: ToDoEdit,
+  },
+  {
+    initialRouteName: 'Product',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#777777',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  },
+);
+const MainContainer = createAppContainer(Main);
+
+export default class App extends React.Component {
   render() {
-    return (
-      <View>
-        <Text>Welcome to CS</Text>
-      </View>
-    );
+    return <MainContainer />;
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
